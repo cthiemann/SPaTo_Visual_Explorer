98c985cbad8598743e21a6b85d1b16004d344e7f
Christian Thiemann <mail@christian-thiemann.de>
2011-08-20 10:58:34 +0200

    [-PDE] Move data import things into place

---
+++ src/net/spato/sve/app/data/DataImportWizard.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2011 Christian Thiemann <christian@spato.net>
+ * Developed at Northwestern University <http://rocs.northwestern.edu>
+ *
+ * This file is part of the SPaTo Visual Explorer (SPaTo).
+ *
+ * SPaTo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPaTo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPaTo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.spato.sve.app.data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import net.spato.sve.app.SPaTo_Visual_Explorer;
+import processing.core.PApplet;
+import tGUI.*;
+
+
+public class DataImportWizard extends TFrame {
+
+  protected TConsole console;
+  protected File file = null;
+  protected String str = null;
+  protected ExecutorService worker = null;
+  protected BufferedReader reader = null;
+  protected String lines[] = null;
+
+  private DataImportWizard() {
+    super(SPaTo_Visual_Explorer.INSTANCE.gui, "Data Import Wizard");
+    setActionEventHandler(this);
+    guiSetup();
+    worker = Executors.newSingleThreadExecutor();
+  }
+
+  public DataImportWizard(File file) {
+    this(); setTitle(file.getName());
+    this.file = file;
+    try {
+      reader = new BufferedReader(new FileReader(file));
+    } catch (Exception e) {
+      console.logError("Could not open file for reading: ", e);
+      e.printStackTrace();
+    }
+  }
+
+  public DataImportWizard(String str) {
+    this(); setTitle("Pasted/dropped text data");
+    this.str = str;
+    try {
+      reader = new BufferedReader(new StringReader(str));
+    } catch (Exception e) {
+      console.logError("Could not read text data: ", e);
+      e.printStackTrace();
+    }
+  }
+
+  public void guiSetup() {
+    setBounds(100, 100, 300, 200);
+    console = gui.createConsole("import");
+    console.setAlignment(TConsole.ALIGN_LEFT);
+    console.setFancy(false);
+    add(console, TBorderLayout.SOUTH);
+  }
+
+  public void start() {
+    validate(); gui.add(this); gui.requestFocus(this);
+    worker.submit(new Runnable() {
+      public void run() {
+        readData();  // or... read first line(s) and detect data type (e.g. GraphML etc)
+        TabulatedData data = new TabulatedData(lines);
+      }
+    });
+  }
+
+  public void readData() {
+    console.logProgress("Reading data").indeterminate();
+    lines = new String[1024];
+    int NL = 0;
+    try {
+      while ((lines[NL] = reader.readLine()) != null)
+        if (++NL >= lines.length)
+          lines = PApplet.expand(lines);
+    } catch (IOException e) {
+      console.abortProgress("Error while reading data: ", e);
+      e.printStackTrace();
+      lines = null;
+    }
+    lines = PApplet.subset(lines, 0, NL);
+    console.finishProgress();
+    console.logNote("Got " + NL + " lines of data");
+  }
+
+  public void finish() {
+    worker.shutdown();
+    gui.remove(this);
+  }
+
+  public void actionPerformed(String cmd) {
+    //
+  }
+
+}
---
+++ src/net/spato/sve/app/data/DataTransferHandler.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2011 Christian Thiemann <christian@spato.net>
+ * Developed at Northwestern University <http://rocs.northwestern.edu>
+ *
+ * This file is part of the SPaTo Visual Explorer (SPaTo).
+ *
+ * SPaTo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPaTo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPaTo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.spato.sve.app.data;
+
+import net.spato.sve.app.SPaTo_Visual_Explorer;
+import java.awt.dnd.DropTargetListener;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTarget;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.io.File;
+import processing.core.PApplet;
+import tGUI.*;
+import java.io.BufferedReader;
+import java.io.Reader;
+
+
+public class DataTransferHandler implements DropTargetListener {
+
+  protected SPaTo_Visual_Explorer app = null;
+  private TWindow winDropStatus = null;
+
+  public DataTransferHandler(SPaTo_Visual_Explorer app) {
+    this.app = app;
+    //
+    winDropStatus = new TWindow(app.gui);
+    winDropStatus.setFocusable(false);
+    winDropStatus.setBackground(app.color(225, 225, 175, 225));
+    winDropStatus.setPadding(15);
+    //
+    new DropTarget(app, DnDConstants.ACTION_COPY, this, true);
+  }
+
+  public boolean canAccept(Transferable t) {
+    return true;//t.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+  }
+
+  private void showDropStatus(String str, Transferable t) {
+    str += "\n\nFlavors:\n";
+    for (DataFlavor f : t.getTransferDataFlavors())
+      str += "    " + f + "\n";
+    Reader reader = null;
+    BufferedReader bufreader = null;
+    try {
+      DataFlavor flavor = DataFlavor.selectBestTextFlavor(t.getTransferDataFlavors());
+      str += "\n\nFlavor: " + flavor + "\n";
+      reader = flavor.getReaderForText(t);
+      str += "\nReader: " + reader + "\n";
+      bufreader = new BufferedReader(reader);
+      str += "BufferedReader: " + bufreader + "\n\n";
+      String line; int i = 0;
+      while ((line = bufreader.readLine()) != null)
+        str += "[" + i + "] " + line + "\n";
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try { bufreader.close(); } catch (Exception e) {}
+    }
+    try {
+      @SuppressWarnings("unchecked")  // FIXME: can we avoid "unchecked cast" here?
+      java.util.List<File> ff = (java.util.List<File>)t.getTransferData(DataFlavor.javaFileListFlavor);
+      str += "\n\nList: " + ff + "\n\n";
+      for (Object f : ff)
+        str += f + "\n";
+    } catch (Exception e) {
+      str += "  XXX  " + e.getMessage();
+    }
+    try {
+      str += "\n\nString: ";
+      String s = (String)t.getTransferData(DataFlavor.stringFlavor);
+      str += s + "\n\n";
+    } catch (Exception e) {
+      str += "  XXX  " + e.getMessage();
+    }
+    //
+    app.println("==========================================================");
+    app.print(str);
+    app.println("==========================================================");
+    synchronized (app.gui) {
+      winDropStatus.removeAll();
+      winDropStatus.add(app.gui.createLabel(str));
+      TComponent.Dimension d = winDropStatus.getPreferredSize();
+      winDropStatus.setBounds(app.width/2 - d.width/2, app.height/2 - d.height/2, d.width, d.height);
+      app.gui.add(winDropStatus);
+    }
+  }
+
+  private void hideDropStatus() {
+    app.gui.remove(winDropStatus);
+  }
+
+  @SuppressWarnings("unchecked")  // FIXME: can we avoid "unchecked cast" at ff = ...getTransferData(...)?
+  public boolean handleTransferable(Transferable t) {
+    // handle dropped files
+    if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+      File ff[];
+      try {
+        ff = ((java.util.List<File>)t.getTransferData(DataFlavor.javaFileListFlavor)).toArray(new File[0]);
+      } catch (Exception e) {
+        app.console.logError("Error while getting the names of the dropped/pasted files: ", e);
+        e.printStackTrace();
+        return false;
+      }
+      handleDroppedFiles(ff);
+      return true;
+    }
+    // handle dropped or pasted text
+    if (t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+      String str = null;
+      try {
+        str = (String)t.getTransferData(DataFlavor.stringFlavor);
+      } catch (Exception e) {
+        app.console.logError("Error while retrieving the dropped/pasted text: ", e);
+        e.printStackTrace();
+        return false;
+      }
+      if (str.startsWith("file://")) {  // Linux reports file drops as lists of file URLs
+        File ff[] = new File[0];
+        for (String line : PApplet.split(str, "\n")) {
+          line = PApplet.trim(line);
+          if ((line.length() > 0) && line.startsWith("file://"))
+            ff = (File[])PApplet.append(ff, new File(line.substring(7)));
+        }
+        handleDroppedFiles(ff);
+      } else
+        new DataImportWizard(str).start();
+      return true;
+    }
+    // don't know what to do
+    return false;
+  }
+
+  public void handleDroppedFiles(File ff[]) {
+    for (File f : ff) {
+      if (f.getName().endsWith(".spato"))
+        app.openDocument(f);
+      else if (f.getName().endsWith(".mat"))
+        ;//FIXME//new Thread(new MatFileImport(f)).start();
+      else
+        new DataImportWizard(f).start();
+    }
+  }
+
+  /*
+   * DropTargetListener methods
+   */
+
+  public void dragEnter(DropTargetDragEvent event) {
+//      showDropStatus(event.toString(), event.getTransferable());
+    event.acceptDrag(DnDConstants.ACTION_COPY);
+  }
+
+  public void dragExit(DropTargetEvent event) {
+//      hideDropStatus();
+  }
+
+  public void dragOver(DropTargetDragEvent event) {
+    event.acceptDrag(DnDConstants.ACTION_COPY);
+  }
+
+  public void drop(DropTargetDropEvent event) {
+    if (canAccept(event.getTransferable())) {
+      event.acceptDrop(DnDConstants.ACTION_COPY);
+      handleTransferable(event.getTransferable());
+//        showDropStatus(event.toString(), event.getTransferable());
+//        hideDropStatus();
+      event.dropComplete(true);
+    } else {
+      event.rejectDrop();
+    }
+    if (!app.focused) app.redraw();  // go back into CPU-cycle saving mode, but erase drop status
+  }
+
+  public void dropActionChanged(DropTargetDragEvent event) {
+    event.acceptDrag(DnDConstants.ACTION_COPY);
+  }
+
+}
